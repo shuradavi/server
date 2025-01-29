1562054271cfec37e79b0915908264ac0cbf54de
shuradavi <shuradavi@gmail.com>
2025-01-29 18:22:32 +0300

    1

--- express.js
+++ express.js
@@ -6,23 +6,30 @@ const fs = require('fs-extra');
 const app = express();
 
 // Определяем порт для прослушивания запросов
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Обработчик GET-запросов к корню сайта
-app.get('/', (req, res) => {
-    // Путь до файла, который нужно отдать клиенту
-    const filePath = path.join(__dirname, 'clients', 'wg0-client-admin2.conf');
+app.get('/file/:filename', (req, res) => {
+    // Получаем имя файла из параметра запроса
+    const filename = req.params.filename;
 
-    // Проверяем существование файла
+    // Путь до файла, который нужно отдать клиенту
+    const filePath = path.join(__dirname, 'clients', filename);
+
+     // Проверяем существование файла
     if (!fs.existsSync(filePath)) {
         return res.status(404).send('Файл не найден.');
     }
 
     // Отправляем файл клиенту
     res.sendFile(filePath);
+
+    // Отправляем файл клиенту
+    // res.download(filePath);
 });
+
 
 // Запускаем сервер
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://45.129.141.143:${PORT}`);
 });
